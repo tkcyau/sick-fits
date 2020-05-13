a77987a17b87b03d2509f7aad5cd7cfc9717f633
Tim <tkcyau@gmail.com>
2020-05-13 15:44:09 -0400

    change signup mutation  permission back to user

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -62,7 +62,7 @@ const Mutations = {
         data: {
           ...args,
           password,
-          permissions: { set: ["ADMIN"] },
+          permissions: { set: ["USER"] },
         },
       },
       info
